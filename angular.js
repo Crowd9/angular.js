516152fe02c9a02367fe15ff63e2efdfb4061475
nskazki <nskazki@gmail.com>
2020-09-07 16:54:10 +0300

    gleam - re-build patched angular 1.8.0 for gallery-v2
        yarn install --check-files
        git tag v1.8.0-patched-for-gallery-v2-by-gleam-v3 -a -m "codename(summer-has-come-to-the-end)"
        yarn grunt build
        cp build/angular.js .

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.8.0-patched-for-gallery-v2-by-gleam-v2
+ * @license AngularJS v1.8.0-patched-for-gallery-v2-by-gleam-v3
  * (c) 2010-2020 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -99,7 +99,7 @@ function isValidObjectMaxDepth(maxDepth) {
 function minErr(module, ErrorConstructor) {
   ErrorConstructor = ErrorConstructor || Error;
 
-  var url = 'https://errors.angularjs.org/1.8.0-patched-for-gallery-v2-by-gleam-v2/';
+  var url = 'https://errors.angularjs.org/1.8.0-patched-for-gallery-v2-by-gleam-v3/';
   var regex = url.replace('.', '\\.') + '[\\s\\S]*';
   var errRegExp = new RegExp(regex, 'g');
 
@@ -2820,11 +2820,11 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.8.0-patched-for-gallery-v2-by-gleam-v2',
+  full: '1.8.0-patched-for-gallery-v2-by-gleam-v3',
   major: 1,
   minor: 8,
   dot: 0,
-  codeName: 'should-I-even-make-up-one'
+  codeName: 'summer-has-come-to-the-end'
 };
 
 
@@ -2973,7 +2973,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.8.0-patched-for-gallery-v2-by-gleam-v2' });
+  .info({ angularVersion: '1.8.0-patched-for-gallery-v2-by-gleam-v3' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
